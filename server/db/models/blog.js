3232657efa11d6b101abb61432bf9de66475e943
Dj <rodriguezdanilo88@gmail.com>
2018-04-16 21:24:11 -0700

    creates tables in DB

---
+++ server/db/models/blog.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Blog = sequelize.define('Blog', {
+    authorId: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    article: DataTypes.STRING,
+    featured: DataTypes.BOOLEAN,
+    published: DataTypes.DATE
+  }, {});
+  Blog.associate = function(models) {
+    // associations can be defined here
+    models.Blog.belongsTo(models.Author, {foreignKey: 'authorId'})
+  };
+  return Blog;
+};
---
+++ server/index.js
@@ -0,0 +1,6 @@
+const server = require('./app');
+const port = process.env.PORT || 8080;
+
+server.listen(port,function(){
+    console.log('Server is listening on 8080')
+})
