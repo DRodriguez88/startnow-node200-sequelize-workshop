f2bd8efd228ed86a5dc24bacb1adbc13a5b0cf88
Dj <rodriguezdanilo88@gmail.com>
2018-04-17 18:49:47 -0700

    All routes for author

--- server/routes/authors.js
+++ server/routes/authors.js
@@ -2,11 +2,52 @@ const express = require('express');
 const router = express.Router();
 const models = require( '../db/models/index');
 
+//works
 router.get('/', (req, res) => {
     models.Author.findAll()
       .then( authors => {
         res.status( 200 ).json( authors )
       });
   });
+
+//works
+router.get('/:id', (req,res) => {
+    models.Author.findById(req.params.id)
+        .then( authors => {
+            res.status(200).json( authors )
+        });
+});
+
+//works
+router.get('/:id/blogs', (req,res) => {
+    models.Blog.findAll({ where: { authorId: req.params.id }})
+        .then( blogs => {
+            res.status(200).json( blogs )
+        });
+});
+
+//works
+router.post('/', (req,res) => {
+    models.Author.create(req.body)
+        .then( author => {
+            res.status(201).json( author )
+        });
+});
+
+router.put('/:id', (req,res) => {
+    models.Author.findById(req.params.id)
+        .then( author => {
+            author.updateAttributes(req.body);
+            res.status(201).json( author )
+        });
+});
+
+router.delete('/:id', (req,res) => {
+    models.Author.findById(req.params.id)
+        .then( author => {
+            author.destroy();
+            res.status(201).json( author )
+        });
+});
 
 module.exports = router;
